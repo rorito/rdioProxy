b57c35f4a45f91fe32d5725fb9d0b33946353247
Rory Petty <rorypetty@gmail.com>
2014-02-04 02:20:34 -0500

    cut to express because hapi's CORS implementation is busted

--- app.js
+++ app.js
@@ -1,44 +1,57 @@
-var Hapi = require('hapi'),
-    Rdio = require('./rdio')
+var express = require('express'),
+  Rdio = require('./rdio')
+  api = express();
 
-var server = new Hapi.Server(+process.env.PORT, '0.0.0.0');
+// api middleware
+api.use(express.logger('dev'));
+api.use(express.bodyParser());
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request) {
-    var rdio = new Rdio([process.env.RDIO_CONSUMER_KEY, process.env.RDIO_CONSUMER_SECRET]);
+/**
+ * CORS support.
+ */
 
-    var newParams = request.query;
-    if(!request.query.user){
-      newParams["user"] = process.env.DEFAULT_USERKEY;
-    }
+api.all('*', function(req, res, next){
+  if (!req.get('Origin')) return next();
+  // use "*" here to accept any origin
+  res.set('Access-Control-Allow-Origin', 'http://localhost:8000');
+  res.set('Access-Control-Allow-Methods', 'GET, POST');
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  // res.set('Access-Control-Allow-Max-Age', 3600);
+  if ('OPTIONS' == req.method) return res.send(200);
+  next();
+});
 
-    var rdioApiCall = "getHeavyRotation"; //default
-    if(request.query.apiCall){
-      rdioApiCall = request.query.apiCall;
-      delete newParams["apiCall"]; //remove this from the params list because it's not an actual rdio param
-    }
+/**
+ * POST a user.
+ */
 
-    console.log("apiCall: " + rdioApiCall);
-    console.log("userKey: " + newParams.user);
-    console.log("params: " + JSON.stringify(newParams));
+api.get('/', function(req, res){
+  var rdio = new Rdio([process.env.RDIO_CONSUMER_KEY, process.env.RDIO_CONSUMER_SECRET]);
 
-    rdio.call(rdioApiCall,newParams, function(err, data) {
-      if (err) {
-        console.log(err);
-        request.reply(new Error("Error getting heavy rotation"));
-      }
-
-      var body = [];
-      body.push(JSON.stringify(data));
-
-      request.reply(body.join('\n'));
-    });
+  //TODO only parse valid querystring params for rdio API instead of just blanketly accepting params?
+  var newParams = req.query;
+  if(!req.query.user){
+    newParams["user"] = process.env.DEFAULT_USERKEY;
   }
+
+  var rdioApiCall = "getHeavyRotation"; //default
+  if(req.query.apiCall){
+    rdioApiCall = req.query.apiCall;
+    delete newParams["apiCall"]; //remove this from the params list because it's not an actual rdio param
+  }
+
+  console.log("apiCall: " + rdioApiCall);
+  console.log("userKey: " + newParams.user);
+  console.log("params: " + JSON.stringify(newParams));
+
+  rdio.call(rdioApiCall,newParams, function(err, data) {
+    if (err) {
+      console.log(err);
+      res.send(new Error("Error getting heavy rotation"));
+    }
+
+    res.json(data);
+  });
 });
 
-server.start(function () {
-  server.settings.uri = process.env.HOST ? 'http://' + process.env.HOST + ':' + process.env.PORT : server.settings.uri;
-  console.log('Server started at ' + server.settings.uri);
-});
+api.listen(process.env.PORT || 5000);
